ec8f81090566e898d60687348e24573dd98e8b41
techadomobi <shubham.adomobi@gmail.com>
2026-04-30 18:09:13 +0000

    pages

--- artifacts/api-server/src/routes/health.ts
+++ artifacts/api-server/src/routes/health.ts
@@ -1,9 +1,9 @@
-import { Router, Request, Response } from "express";
+import { Router, type Request } from "express";
 import { HealthCheckResponse } from "@workspace/api-zod";
 
 const router = Router();
 
-router.get("/healthz", (_req: Request, res: Response) => {
+router.get("/healthz", (_req: Request, res) => {
   const data = HealthCheckResponse.parse({ status: "ok" });
   res.json(data);
 });
